4f165cc7de60e795dc30b869f0ff7a07a56ccf1f
Tinkith <timurabduvaliev6@gmail.com>
2026-04-23 23:38:08 +0300

    Skeleton

---
+++ cmd/sessionctl/main.go
@@ -0,0 +1,26 @@
+package main
+
+// import (
+//
+//	"flag"
+//	"fmt"
+//	"os"
+//
+// )
+func main() {
+	//	ptr_string := flag.String("dir", "", "Path to the PHP sessions directory")
+	//	flag.Parse()
+	//	value := *ptr_string
+	//
+	//	if value == "" {
+	//		flag.Usage()
+	//		os.Exit(1)
+	//	}
+	//
+	//	info, err := os.Stat(value)
+	//	if err != nil {
+	//		fmt.Fprintln(os.Stderr, err)
+	//		os.Exit(1)
+	//	}
+	//	fmt.Println("OK")
+}
